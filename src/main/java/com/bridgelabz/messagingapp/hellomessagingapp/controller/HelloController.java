ea02c7c8136169d40e4d87a21bce39e5a150bc68
Abhishek Magdum <abhishek.magdum.07@gmail.com>
2023-06-08 16:14:22 +0530

    Added : Sending hello message to the user using requestParam method.

--- src/main/java/com/bridgelabz/messagingapp/hellomessagingapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/messagingapp/hellomessagingapp/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.messagingapp.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    //Uc1 - Create rest controller to show hello message to the user.
     @GetMapping("/hello")
     public String helloMessage(){
         return "Hello from Bridgelabz";
     }
+    @GetMapping("/get")
+    public String requestParam(@RequestParam String firstName){
+        return "Hello "+firstName+" from Bridgelabz";
+    }
+
+
 }
